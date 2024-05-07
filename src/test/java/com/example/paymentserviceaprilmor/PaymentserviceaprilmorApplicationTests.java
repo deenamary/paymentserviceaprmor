35d8c88b7fdf5e4b9be1d603721d781e6d15af45
deena <deenaantonym@gmail.com>
2024-05-07 17:57:18 +0530

    Initial commit

---
+++ src/test/java/com/example/paymentserviceaprilmor/PaymentserviceaprilmorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentserviceaprilmor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentserviceaprilmorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
